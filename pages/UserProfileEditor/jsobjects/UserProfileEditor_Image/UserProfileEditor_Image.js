9be5bcd21cfa4ee2abd0e34be97138aa8706701e
João Pedro Oliveira <jotape1662003@gmail.com>
2025-05-28 20:44:16 +0000

    fix

--- pages/UserProfileEditor/jsobjects/UserProfileEditor_Image/UserProfileEditor_Image.js
+++ pages/UserProfileEditor/jsobjects/UserProfileEditor_Image/UserProfileEditor_Image.js
@@ -1,7 +1,7 @@
 export default {
 	fpk_selectImageonFilesSelected () {
 		if (ProfileEditor_FileUpload.data.url && ProfileEditor_FileUpload.data.url.trim().length > 0) {
-			img_uploadImage.setImage(ProfileEditor_FileUpload.data.url)
+			img_uploadImage.setImage(ProfileEditor_FileUpload.data.url);
 			storeValue("tempUserPhoto",ProfileEditor_FileUpload.data.url);
 		}
 		else { showAlert("Erro ao fazer o upload da imagem. URL não encontrado.", "error"); }

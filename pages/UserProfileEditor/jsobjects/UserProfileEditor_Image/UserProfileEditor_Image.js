e013e7ec2137f8c35bc99771af94821c79ceae1c
João Pedro Oliveira <jotape1662003@gmail.com>
2025-05-29 18:44:14 +0000

    ok

--- pages/UserProfileEditor/jsobjects/UserProfileEditor_Image/UserProfileEditor_Image.js
+++ pages/UserProfileEditor/jsobjects/UserProfileEditor_Image/UserProfileEditor_Image.js
@@ -1,8 +1,8 @@
 export default {
 	selectImageonFilesSelected() {
-		if (ProfileEditor_ImageUpload.data.data.url && ProfileEditor_Upload.data.data.url.trim().length > 0) {
-			img_uploadImage.setImage(ProfileEditor_Upload.data.data.url);
-			storeValue("tempUserPhoto",ProfileEditor_Upload.data.data.url);
+		if (ProfileEditor_ImageUpload.data.data.url && ProfileEditor_ImageUpload.data.data.url.trim().length > 0) {
+			img_uploadImage.setImage(ProfileEditor_ImageUpload.data.data.url);
+			storeValue("tempUserPhoto",ProfileEditor_ImageUpload.data.data.url);
 		}
 		else { showAlert("Erro ao fazer o upload da imagem. URL não encontrado.", "error"); }
 	},
